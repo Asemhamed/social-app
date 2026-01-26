8d7d137a0a2fedfa1ddfeeaa5ea675963382bc5d
Asemhamed <asmhmd789@gmail.com>
2026-01-26 14:25:51 +0200

    Lastttt

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
-  base:"/social-app/"
+  plugins: [react(), tailwindcss(), flowbiteReact()]
 })
